26304be37492a0b7650f82dccb25f9486a2d2b72
Beladiyamitul <mitulbeladiya812@gmail.com>
2022-11-21 17:32:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+// ====== Create New File ========
+
+// fs.writeFileSync("createfile.txt" , "welcome")
+// fs.writeFileSync("cratefile.txt" , "welcome to node js")
+
+// fs.appendFileSync("cratefile.txt" , "How Are You I Am Fine")
+
+// ======== READ FILE DATA ==========
+
+// const buf_data = fs.readFileSync('createfile.txt')
+
+// org_data = buf_data.toString();
+
+// console.log(org_data);
+
+// ========== RENAME FILE ==========
+
+// fs.renameSync("createfile.txt" , "createnew.txt" )
+
+// ========== Delete FILE ==========
+
+// fs.unlinkSync("createnew.txt")
+
+
+// ======= Only Practice ===========
+
+
+// fs.writeFileSync("readfile.txt" , "Tsting file")
+
+// fs.appendFileSync("readfile.txt" , "How Are You")
+
+// const readonly = fs.readFileSync("readfile.txt", "utf8")
+
+// fs.renameSync("readfile.txt" , "readingfile.txt")
+
+// fs.unlinkSync("readingfile.txt")
+
+
+
+
+
+
+
