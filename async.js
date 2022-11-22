e4b5068150e480763039a9d55f13216545e81ef1
Beladiyamitul <mitulbeladiya812@gmail.com>
2022-11-22 10:01:54 +0530

    Asyncro-file-system

--- async.js
+++ async.js
@@ -3,3 +3,15 @@ const fs = require("fs");
 fs.writeFile("read.txt" , "hello ,  your work is good" , (err) =>{
     console.log("created Files");
 })
+
+fs.appendFile("read.txt" , 'How are you' , (err) =>{
+    console.log("complete add");
+})
+ 
+fs.readFile("read.txt" , "utf-8" , (err, data)=>{
+    console.log(data);
+})
+
+fs.rename("read.txt" , "reading.txt" , (err) =>{
+    console.log("complete file name changes");
+})  
